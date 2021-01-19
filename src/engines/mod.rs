463c2a3ccf13f48c3f7928d03344b39d421b302b
Diretnan Domnan <diretnandomnan@gmail.com>
2021-01-19 14:26:02 +0100

    specified rust version using rust-toolchain

--- src/engines/mod.rs
+++ src/engines/mod.rs
@@ -7,26 +7,26 @@ use crate::utils::render_select_music;
 use log::error;
 
 pub async fn search_all(engine: &str, query: &str) -> MythraResult<Vec<Music>> {
-  let query = String::from(query);
-  match engine {
-    "freemp3cloud" => {
-      let e = freemp3cloud::FreeMP3Cloud {};
-      e.search(query).await
+    let query = String::from(query);
+    match engine {
+        "freemp3cloud" => {
+            let e = freemp3cloud::FreeMP3Cloud {};
+            e.search(query).await
+        }
+        "mp3s" => {
+            let e = mp3s::MP3S {};
+            e.search(query).await
+        }
+        _ => {
+            let empty: Vec<Music> = vec![];
+            error!("Engine is unsupported");
+            Ok(empty)
+        }
     }
-    "mp3s" => {
-      let e = mp3s::MP3S {};
-      e.search(query).await
-    }
-    _ => {
-      let empty: Vec<Music> = vec![];
-      error!("Engine is unsupported");
-      Ok(empty)
-    }
-  }
 }
 
 pub async fn cli(engine: &str, query: &str) {
-  let title: &str = &(format!("Searching {} for {}", engine, query))[..];
-  let results = search_all(engine, query).await.ok().unwrap();
-  render_select_music(results, title);
+    let title: &str = &(format!("Searching {} for {}", engine, query))[..];
+    let results = search_all(engine, query).await.ok().unwrap();
+    render_select_music(results, title);
 }
