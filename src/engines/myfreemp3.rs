2a9134844adac6a0333b0d2dbd5e73b1e5e9d040
Wisdom Praise <wisdompraise968@gmail.com>
2021-01-21 16:55:21 +0100

    Still working on the freeMp3 engine

--- src/engines/myfreemp3.rs
+++ src/engines/myfreemp3.rs
@@ -3,8 +3,11 @@ use crate::utils::cached_reqwest;
 use crate::utils::extract_from_el;
 
 use indicatif::ProgressBar;
-use log::debug;
+use log::{debug, info};
 use scraper::{ElementRef, Html, Selector};
+use serde_json::{Value};
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
 
 
 pub struct MyFreeMP3;
@@ -14,13 +17,70 @@ pub static CONFIG: Engine = Engine {
     search_url: "https://my-free-mp3.vip/api/search.php",
 };
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Data {
+    response: Vec<Test>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Arr {
+    First(String),
+    Vector(Test)
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Test {
+   name: String
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct Elem {
+    artist: String,
+    id: u64,
+    owner_id: u64,
+    title: String,
+    access_key: String,
+    is_licenced: bool,
+    url: String,
+    date: u64,
+    album: Album,
+    short_video_allowed: bool,
+    stories_allowed: bool,
+    stories_cover_allowed: bool
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct Album {
+    id: usize,
+    title: String,
+    owner_id: i64,
+    access_key: String,
+    thumb: Thumb
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct Thumb {
+    width: i64,
+    height: i64,
+    photo_34: String,
+    photo_68: String,
+    photo_135: String,
+    photo_270: String,
+    photo_300: String,
+    photo_600: String,
+    photo_1200: String,
+}
+
 impl MyFreeMP3 {
     pub async fn search(&self, _query: String) -> MythraResult<Vec<Music>> {
         let mut _query= String::from(&_query[..]);
         let bar = ProgressBar::new(100);
         let full_url: String = CONFIG.search_url.to_owned();
-        let res = cached_reqwest::post(&full_url,&_query).await;
-        debug!("Retrieving song with response -> {:?}", res);
+        let form_data = [
+                        ("q", _query.as_str()),
+                        ("sort", "2"),
+                        ("page", "0"),
+                    ];
+        let res = cached_reqwest::post(&full_url,&form_data).await.ok().unwrap();
+        //info!("Retrieving song with response -> {:?}", res);
+        let v: Data = self.typed_example(&res).ok().unwrap();
+        println!("Retrieving song with data -> {:?}", v);
         let document = Html::parse_document("test");
         let selector = Selector::parse(".unplaying").unwrap();
         let mut vec: Vec<Music> = Vec::new();
@@ -39,6 +99,23 @@ impl MyFreeMP3 {
         bar.finish();
 
         Ok(vec)
+    }
+    pub fn typed_example(&self, data: &String) -> Result<Data> {
+        let data = r#"
+            {
+                "response": [
+                    "apple",
+                    {
+                        "name": "wisdom"
+                    }
+                ]
+            }"#;
+        let d: Data = serde_json::from_str(data)?;
+
+        // Do things just like with any other Rust data structure.
+        println!("Data {:?}", d);
+
+        Ok(d)
     }
 
     pub async fn parse_single_music(&self, ind: usize, element: ElementRef<'_>) -> Option<Music> {
