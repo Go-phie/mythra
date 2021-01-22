24ab71769d049d8949205dbba6efa221059ac8c5
Wisdom Praise <wisdompraise968@gmail.com>
2021-01-22 16:42:31 +0100

    Parsed the json response but issues with looping through vectors

--- src/engines/myfreemp3.rs
+++ src/engines/myfreemp3.rs
@@ -1,3 +1,4 @@
+use serde_json::Map;
 use crate::types::{Engine, Music, MythraResult};
 use crate::utils::cached_reqwest;
 use crate::utils::extract_from_el;
@@ -6,9 +7,8 @@ use indicatif::ProgressBar;
 use log::{debug, info};
 use scraper::{ElementRef, Html, Selector};
 use serde_json::{Value};
-use serde::{Deserialize, Serialize};
 use serde_json::Result;
-
+use regex::Regex;
 
 pub struct MyFreeMP3;
 pub static CONFIG: Engine = Engine {
@@ -17,55 +17,6 @@ pub static CONFIG: Engine = Engine {
     search_url: "https://my-free-mp3.vip/api/search.php",
 };
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Data {
-    response: Vec<Test>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub enum Arr {
-    First(String),
-    Vector(Test)
-}
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Test {
-   name: String
-}
-#[derive(Serialize, Deserialize, Debug)]
-struct Elem {
-    artist: String,
-    id: u64,
-    owner_id: u64,
-    title: String,
-    access_key: String,
-    is_licenced: bool,
-    url: String,
-    date: u64,
-    album: Album,
-    short_video_allowed: bool,
-    stories_allowed: bool,
-    stories_cover_allowed: bool
-}
-#[derive(Serialize, Deserialize, Debug)]
-struct Album {
-    id: usize,
-    title: String,
-    owner_id: i64,
-    access_key: String,
-    thumb: Thumb
-}
-#[derive(Serialize, Deserialize, Debug)]
-struct Thumb {
-    width: i64,
-    height: i64,
-    photo_34: String,
-    photo_68: String,
-    photo_135: String,
-    photo_270: String,
-    photo_300: String,
-    photo_600: String,
-    photo_1200: String,
-}
 
 impl MyFreeMP3 {
     pub async fn search(&self, _query: String) -> MythraResult<Vec<Music>> {
@@ -79,15 +30,15 @@ impl MyFreeMP3 {
                     ];
         let res = cached_reqwest::post(&full_url,&form_data).await.ok().unwrap();
         //info!("Retrieving song with response -> {:?}", res);
-        let v: Data = self.typed_example(&res).ok().unwrap();
-        println!("Retrieving song with data -> {:?}", v);
-        let document = Html::parse_document("test");
-        let selector = Selector::parse(".unplaying").unwrap();
+        let v: Value = self.format_response(&res).ok().unwrap().clone();
+        //println!("Retrieving song with data -> {:?}", v);
         let mut vec: Vec<Music> = Vec::new();
-        let elems = document.select(&selector);
-        let size = elems.count();
-        for (ind, element) in document.select(&selector).enumerate() {
-            let single_music = self.parse_single_music(ind, element).await;
+        let elems = v["response"].as_array().unwrap();
+        let size = elems.len();
+        println!("size: {:?}", size);
+        for (ind, &element) in v["response"].as_array().clone().iter().enumerate() {
+            println!("element: {:?}", &element);
+            let single_music = self.parse_single_music(ind, &element[0].as_object()).await;
             match single_music {
                 Some(music) => vec.push(music),
                 _ => (),
@@ -100,30 +51,27 @@ impl MyFreeMP3 {
 
         Ok(vec)
     }
-    pub fn typed_example(&self, data: &String) -> Result<Data> {
-        let data = r#"
-            {
-                "response": [
-                    "apple",
-                    {
-                        "name": "wisdom"
-                    }
-                ]
-            }"#;
-        let d: Data = serde_json::from_str(data)?;
+    pub fn format_response(&self, data: &String) -> Result<Value> {
+        
+        //println!("Data before: {:?}", data);
+        let new_data = data.as_str();
+        let new_data = &new_data.replace("\"apple\",", "");
+        //println!("Data response: {:?}", new_data);
+        let re = Regex::new(r"^(?P<last>[(])(?P<content>.*)(?P<first>[)][;]$)").unwrap();
+        let result = re.replace(new_data.as_str(), "$content");
+        //println!("Data response: {:?}", result);
+        let d: Value = serde_json::from_str(result.into_owned().as_str()).unwrap();
 
         // Do things just like with any other Rust data structure.
-        println!("Data {:?}", d);
-
         Ok(d)
     }
 
-    pub async fn parse_single_music(&self, ind: usize, element: ElementRef<'_>) -> Option<Music> {
-        let title = extract_from_el(&element, ".unselectable", "text");
-        let duration = extract_from_el(&element, ".mp3list-bitrate", "text");
-        let selector = Selector::parse(".mp3list-download").unwrap();
-        let tag = element.select(&selector).next().unwrap();
-        let download_link = extract_from_el(&tag, "a", "href");
+    pub async fn parse_single_music(&self,ind :usize, element: &Option<&Map<String, Value>>) -> Option<Music> {
+        //println!("element {:#?}", element.unwrap());
+        let title = element.unwrap().get("title").unwrap().as_str().unwrap().to_owned();
+        //println!("{:#?}", title);
+        let duration = element.unwrap().get("duration").unwrap().as_i64().unwrap().to_string();
+        let download_link = element.unwrap().get("url").unwrap().as_str().unwrap().to_owned();
         debug!("Retrieving song with title -> {}", title);
 
         Some(Music {
