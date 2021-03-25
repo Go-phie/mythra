6ffb0180beb20f5bf4e91017f46a1c611b8531b8
Diretnan Domnan <diretnandomnan@gmail.com>
2021-03-25 21:28:34 +0100

    stop response:null from caching

--- src/utils.rs
+++ src/utils.rs
@@ -249,7 +249,9 @@ pub mod cached_reqwest {
                 let (mut file, contents) = create_or_retrieve(concat_url, exe_path);
                 // if file is empty then cache does not exist
                 // then retrieve directly using reqwest
-                if (contents.as_str()).eq("") || (contents.as_str()).eq("error code: 1020"){
+                if (contents.as_str()).eq("") || 
+                    (contents.as_str()).eq("error code: 1020") ||
+                    (contents.as_str()).eq("{\"response\":null}"){
                     let res = reqwest::Client::new()
                         .post(url)
                         .form(&params).send()
